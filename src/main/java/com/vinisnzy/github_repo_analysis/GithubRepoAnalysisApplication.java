2bc831cf1f6f65445491352d017f0f73e11b4757
vinisnzy <manoseilacara123@gmail.com>
2025-12-15 19:05:09 -0300

    feat: initial commit

---
+++ src/main/java/com/vinisnzy/github_repo_analysis/GithubRepoAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.vinisnzy.github_repo_analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepoAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRepoAnalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinisnzy/github_repo_analysis/GithubRepoAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinisnzy.github_repo_analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepoAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
