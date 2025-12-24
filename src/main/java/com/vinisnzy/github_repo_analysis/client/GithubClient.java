3856716e581691a52bccd6e5756c0447251847be
vinisnzy <manoseilacara123@gmail.com>
2025-12-24 18:48:41 -0300

    feat: add github access token to get repository issues and PRs

--- src/main/java/com/vinisnzy/github_repo_analysis/client/GithubClient.java
+++ src/main/java/com/vinisnzy/github_repo_analysis/client/GithubClient.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.vinisnzy.github_repo_analysis.client.dto.response.GetRepositoryCommitsResponse;
@@ -13,7 +14,7 @@ import com.vinisnzy.github_repo_analysis.client.dto.response.GetRepositoryIssues
 import com.vinisnzy.github_repo_analysis.client.dto.response.GetRepositoryOverviewResponse;
 import com.vinisnzy.github_repo_analysis.client.dto.response.GetRepositoryPRsResponse;
 
-@FeignClient(name = "githubClient", url = "https://api.github.com")
+@FeignClient(name = "githubClient", url = "${github.api.base-url}")
 public interface GithubClient {
 
         @GetMapping("/repos/{owner}/{repo}")
@@ -26,13 +27,13 @@ public interface GithubClient {
 
         @GetMapping("/repos/{owner}/{repo}/pulls")
         List<GetRepositoryPRsResponse> getRepositoryPRs(@PathVariable String owner, @PathVariable String repo,
-                        @RequestParam("state") String state,
-                        @RequestParam(value = "since", required = false) String since);
+                        @RequestParam(value = "since", required = false) String since,
+                        @RequestHeader("Authorization") String GITHUB_TOKEN);
 
         @GetMapping("/repos/{owner}/{repo}/issues")
         List<GetRepositoryIssuesResponse> getRepositoryIssues(@PathVariable String owner, @PathVariable String repo,
-                        @RequestParam("state") String state,
-                        @RequestParam(value = "since", required = false) String since);
+                        @RequestParam(value = "since", required = false) String since,
+                        @RequestHeader("Authorization") String GITHUB_TOKEN);
 
         @GetMapping("/repos/{owner}/{repo}/contents/{path}")
         GetRepositoryFile getRepositoryFile(@PathVariable String owner, @PathVariable String repo,
