25dba087ead613354509552f83dcc29ebcd45754
vinisnzy <manoseilacara123@gmail.com>
2025-12-24 18:51:19 -0300

    fix: match the response fields of the request in the GitHub API

---
+++ src/main/java/com/vinisnzy/github_repo_analysis/client/dto/response/CommitResponse.java
@@ -0,0 +1,4 @@
+package com.vinisnzy.github_repo_analysis.client.dto.response;
+
+public record CommitResponse(CommitterResponse committer) {
+}
--- src/main/java/com/vinisnzy/github_repo_analysis/client/dto/response/GetRepositoryCommitsResponse.java
+++ src/main/java/com/vinisnzy/github_repo_analysis/client/dto/response/GetRepositoryCommitsResponse.java
@@ -1,7 +1,4 @@
 package com.vinisnzy.github_repo_analysis.client.dto.response;
 
-public record GetRepositoryCommitsResponse(
-    String sha,
-    UserResponse author,
-    UserResponse commiter) {
+public record GetRepositoryCommitsResponse(String sha, CommitResponse commit) {
 }
